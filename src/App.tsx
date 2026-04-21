943ce08a7a174e7cdb25d715cbacc421727e93dd
Sabrina <sabrinah_38@hotmail.fr>
2026-04-21 16:17:14 +0200

    feat: define Article type and enhance ResultArticle component with detailed article rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import { useState } from "react";
 
+type Article = {
+  id: number;
+  title: string;
+  published_at: string;
+  user: {
+    name: string;
+  };
+  tag_list: string[];
+};
+
 const ResultArticle = () => {
-  const [results, setResults] = useState([]);
+  const [results, setResults] = useState<Article[]>([]);
 
   const getArticles = async () => {
     try {
@@ -14,7 +24,16 @@ const ResultArticle = () => {
       console.error("Erreur de chargement:", error);
     }
   };
-  const listItems = results.map((item) => <li key={item}>{item}</li>);
+  const formatdate = (published_at: string) => {
+    return new Date(published_at).toLocaleString("en-GB").replace(",", "");
+  };
+
+  const listItems = results.map((item) => (
+    <li key={item.id}>
+      {item.title} - {formatdate(item.published_at)} - {item.user.name} -
+      {item.tag_list.join(", ")}
+    </li>
+  ));
 
   return (
     <>
